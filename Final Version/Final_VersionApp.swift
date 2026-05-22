4423546866cad5683a04c9157d81d5ead26eb0a9
Marcelloguida00 <146440261+Marcelloguida00@users.noreply.github.com>
2026-05-22 21:49:27 +0200

    Initial Commit

---
+++ Final Version/Final_VersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_VersionApp.swift
+//  Final Version
+//
+//  Created by Marcello Guida on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Final_VersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
